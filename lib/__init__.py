714dc447ef24bb2f834e278d98767334e8255617
drtwisted <kirill.omelchenko@gmail.com>
2016-03-20 13:16:26 +0200

    Add lib.py:get_rand to get random numbers of specific range

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,7 +1,9 @@
-from random import sample
+from random import randrange, sample, seed
+from time import clock
 
 
 def get_random_numbers(top_margin, count):
+    seed(clock())
     res = list()
 
     if count > top_margin:
@@ -13,3 +15,9 @@ def get_random_numbers(top_margin, count):
         res = sample(range(top_margin), count)
 
     return res
+
+
+def get_rand(top_margin, low_margin=0):
+    seed(clock())
+    top_margin = top_margin if top_margin >= low_margin else low_margin
+    return randrange(low_margin, top_margin)
